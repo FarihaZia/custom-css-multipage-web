27ec6e64560a21b4493f9a69c7be0d82ba889a26
Fariha zia <Farihazia2006@gmail.com>
2024-12-15 22:20:10 +0500

    initial commit

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 
-export const About = () => {
+ const About = () => {
   return (
     <div className="page about">
     <h1>About Us</h1>
     <p>Learn more about our team and mission on this page.</p>
   </div>
-  )
-}
-export default About
+  )}
+export default About 
